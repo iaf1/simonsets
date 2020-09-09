ac3d338ec7068408cfcb4e72addb92f5f3253dc6
Simons Git <simon.cichy@outlook.com>
2020-09-09 13:03:31 +0200

    updating the is_set function and implementing the loop for all card combinations in detection

--- detection.py
+++ detection.py
@@ -54,7 +54,7 @@ for idx in range(len(contours[0])):
     
     perc_nonzero = np.count_nonzero(mask) / (mask.shape[0]*mask.shape[1])
     
-    if perc_nonzero < 0.01 : continue
+    if perc_nonzero < 0.01: continue
 
     out = np.zeros_like(img) # Extract out the object and place into output image
     out[mask == 255] = img[mask == 255]
@@ -62,11 +62,19 @@ for idx in range(len(contours[0])):
     cropped_img = np.where(mask == 255)
 
     # Show the output image
-    cv2.imshow('Output', cv2.resize(out, (750,1000)))
+    cv2.imshow('Output', cv2.resize(out, (750, 1000)))
     tup_props = classification(out)
     num, shape, col, fil = tup_props
     print('Number: {n} | Shape: {s} | Color: {c} | Filling: {f}'.format(c=COLORS[col], f=FILLS[fil], s=SHAPES[shape], n=str(num)))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+ 
     board.append(Card(tup_props))
+
+for ii in range(len(board)-2):
+    for iii in range(len(board)-2-ii):
+        for iv in range(len(board)-2-ii-iii):
+            selected = [board[ii], board[ii + iii + 1], board[ii + iii + iv + 2]]
+            is_set(selected)
+
+# seleted = [board[0], board[1], board[2]]
--- functions.py
+++ functions.py
@@ -4,7 +4,9 @@
 Created on Sun Sep  6 19:33:31 2020
 
 @author: ivan
+edit: simon
 """
+import numpy as np
 
 inv_dict = lambda dct: {v: k for (k, v) in dct.items()}
 
@@ -22,6 +24,7 @@ INV_AMOUNTS = inv_dict(AMOUNTS)
 CHR_FILLS = {1: '□ ', 2: '⬔', 3: '■'}
 CHR_SHAPES = {1: '⬭', 2: '～', 3: '▭'}
 CHR_COLORS = {1: 'R', 2: 'G', 3: 'B'}
+
 
 class Card:
     def __init__(self, array):
@@ -50,9 +53,48 @@ class Card:
         
     def chars(self):
         return str(self.array[0]) + CHR_SHAPES.get(self.array[1],'') + CHR_COLORS.get(self.array[2],'') + CHR_FILLS.get(self.array[3],'')
-    
+
+'''
+params = [[□, ⬔, ■], [R, G, B], [⬭, ～, ▭], [1, 2, 3]]'''
 def is_set(inp):
     assert isinstance(inp, list)
-    assert len(inp) == 4
-    
+    assert len(inp) == 3
+    params1 = np.array([np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3)])
+    params2 = np.array([np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3)])
+    params3 = np.array([np.zeros(3), np.zeros(3), np.zeros(3), np.zeros(3)])
+    params1[0][inp[0].array[0] - 1] = 1
+    params1[1][inp[0].array[1] - 1] = 1
+    params1[2][inp[0].array[2] - 1] = 1
+    params1[3][inp[0].array[3] - 1] = 1
+    params2[0][inp[1].array[0] - 1] = 1
+    params2[1][inp[1].array[1] - 1] = 1
+    params2[2][inp[1].array[2] - 1] = 1
+    params2[3][inp[1].array[3] - 1] = 1
+    params3[0][inp[2].array[0] - 1] = 1
+    params3[1][inp[2].array[1] - 1] = 1
+    params3[2][inp[2].array[2] - 1] = 1
+    params3[3][inp[2].array[3] - 1] = 1
+    result = params1 + params2 + params3
+    twos = np.where(result == 2)
+    if twos[0].size == 0:
+        print('is a set')
+        print('1st card: ')
+        print('Number: {n} | Shape: {s} | Color: {c} | Filling: {f}'
+              .format(c=inp[0].color, f=inp[0].fill, s=inp[0].shape, n=inp[0].amount))
+        print('2nd card: ')
+        print('Number: {n} | Shape: {s} | Color: {c} | Filling: {f}'
+              .format(c=inp[1].color, f=inp[1].fill, s=inp[1].shape, n=inp[1].amount))
+        print('3rd card: ')
+        print('Number: {n} | Shape: {s} | Color: {c} | Filling: {f}'
+              .format(c=inp[2].color, f=inp[2].fill, s=inp[2].shape, n=inp[2].amount))
+    # else:
+    #    print('is a set')
+
+
+
+example1 = [Card([3, 1, 1, 1]), Card([3, 1, 1, 2]), Card([3, 1, 1, 3])]
+example2 = [Card([3, 1, 1, 1]), Card([3, 1, 1, 2]), Card([3, 1, 1, 2])]
+is_set(example2)
+
+
         
